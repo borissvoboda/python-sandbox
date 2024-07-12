08aa1aa4076b159171a6e6c7267081a4269fb504
borissvoboda <svoboda.boris@gmail.com>
2024-07-12 16:09:16 +0200

    single graph

---
+++ 01-graph/01-simpleGraph.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Data for plotting
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Create the plot
+plt.plot(x, y)
+
+# Add title and labels
+plt.title("Simple Line Graph")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
+# Display the plot
+plt.show()
